26b7131275d9533617913d01650d47e30a4549d2
Bamiayo17 <@myemail.com>
2023-02-23 16:44:38 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,15 +10,15 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (i = 0; i < 10; i++)
-		{
-		for (j = 0; j < 15; j++)
-		{
-		if (j >= 10)
-		_putchar(j / 10 + '0');
-		_putchar(j % 10 + '0');
-		}
-		}
-		_putchar('\n');
+	for (i = 0; i < 10; i++)
+	{
+	for (j = 0; j < 15; j++)
+	{
+	if (j >= 10)
+	_putchar(j / 10 + '0');
+	_putchar(j % 10 + '0');
+	}
+	}
+	_putchar('\n');
 	}
 }
