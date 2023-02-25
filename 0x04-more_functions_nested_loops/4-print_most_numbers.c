e2e94016e2389ce39d663b10e15302a7fc6392d4
Bamiayo17 <@myemail.com>
2023-02-24 17:14:26 -0800

    4-print_most_numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,22 +1,23 @@
-#inclde "main.h"
+#include "main.h"
 /**
  * print_most_numbers - checks for a digit (0 through 9, excluding 2 and 4
  *
  *Return: Always 0.
  */
-voidprint_most_numbers(void)
+void print_most_numbers(void)
 {
 	int c;
 
 	for (c = 48; c < 58; c++)
 	{
-	if (c != 50)
+		if (c != 50)
 	{
-	if (c != 52)
+		if (c != 52)
 	{
-	_putchar(c);
+		_putchar(c);
 	}
 	}
 	}
 	_putchar('\n');
+}
 
